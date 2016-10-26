0e803c88fb14e99d9e5e3207423cec29fddd53c9
mihajlo <mihajlo88@gmail.com>
2016-10-26 16:33:36 +0200

    boje za eventove

--- content.php
+++ content.php
@@ -1,21 +1,14 @@
-<script type="text/javascript">
-    
-let podaci = [
-{    
-    grad:"beograd",
-    mesto:"dom omladine",
-    pocetak:"10:30",
-    datum:"2016-10-07",
-    ucesnici:"neki tamo",
-    naziv:"glavni naslov",
-    opis:"Opis dogadjaja koji se desava",
-    link:"http://www.google.com"
-}
-];
+<style type="text/css">
+    .boja1{background-color: red;}
+    .boja2{background-color: blue;}
+    .boja3{background-color: green;}
+    .boja4{background-color: grey;}
+    .boja5{background-color: orange;}
+    .boja6{background-color: yellow;}
+    .boja6{background-color: brown;}
 
+</style>
 
-
-</script>
 
 
 <div id="content-inner">
@@ -56,52 +49,70 @@ let podaci = [
                     
                 $unosi = "";
 
-                foreach ($intervali as $int_key=>$interval_value) {
-                   
+                //sortiranje po vremenu u jednom danu
+                ksort($intervali);
 
-                    foreach ($interval_value as  $value) {
-                        $id = md5($value->naziv);
+                    foreach ($intervali as $int_key=>$interval_value) {
+                       
 
-                    $unosi .= <<<UNOS
-                       <span class="event ev_2  ev_2_sub_2"><a href="{$value->link}" class="name" id="$id">{$value->naziv}<span class="vs">{$value->mesto}</span><span class="event-evpeople">{$value->opis}</span></a>
-                        </span>  
+                        foreach ($interval_value as  $value) {
+                            $id = md5($value->naziv);
+
+                        switch ($value->grad) {
+                            case "Beograd": $boja = 'boja1'; break;
+                            case "Nis": $boja = 'boja2'; break;
+                            case "Subotica": $boja = 'boja3'; break;
+                            case "Novi Sad": $boja = 'boja4'; break;
+                            case "Uzice": $boja = 'boja5'; break;
+                            case "Prijepolje": $boja = 'boja6'; break;
+                            case "Leskovac": $boja = 'boja7'; break;
+                             
+                                
+                                default:
+                                    # code...
+                                    break;
+                            }    
+
+                        $unosi .= <<<UNOS
+                           <span class="event $boja"><a href="{$value->link}" class="name" id="$id">{$value->naziv}<span class="vs">{$value->mesto}</span><span class="event-evpeople">{$value->opis}</span></a>
+                            </span>  
 UNOS;
                     }
 
                   
 
 
-                $jedan_period = <<<PERIOD
-                    <h3>$int_key</h3>
-                        <div class="container">
-                            <div class="container-inner">
-                                $unosi  
+                    $jedan_period = <<<PERIOD
+                        <h3>$int_key</h3>
+                            <div class="container">
+                                <div class="container-inner">
+                                    $unosi  
 
-                            <br style="clear:both"> </div>
-                            </div>
+                                <br style="clear:both"> </div>
+                                </div>
 PERIOD;
 
-                $periodi .= $jedan_period;
+                    $periodi .= $jedan_period;
 
-                }
+                    }
 
-                    //ok ima samo niz dana i datuma, tako da nije previse tesko
-                    $dan_nedelja = "";
-                    $dan_mesec = $dan_key ;
+                        //ok ima samo niz dana i datuma, tako da nije previse tesko
+                        $dan_nedelja = "";
+                        $dan_mesec = $dan_key ;
 
-                   $main_template = <<<MAIN
-                    <a class="container-anchor" id="$datum"></a>
+                       $main_template = <<<MAIN
+                        <a class="container-anchor" id="$datum"></a>
 
-                        <div class="container-header ">
-                            <div class="container-dates">
-                                <div class="current-date"><b>$dan_nedelja</b> $dan_mesec</div>
+                            <div class="container-header ">
+                                <div class="container-dates">
+                                    <div class="current-date"><b>$dan_nedelja</b> $dan_mesec</div>
+                                </div>
                             </div>
-                        </div>
-                        <div class="container-top">&nbsp;</div>
+                            <div class="container-top">&nbsp;</div>
 
-                        $periodi
+                            $periodi
 
-                        
+                            
 
 MAIN;
                    $ceo_tekst .= $main_template;
