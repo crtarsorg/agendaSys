b4dae4bac6b67fa3da37756caedb7ecafc0a0e92
XoDev <misha@xodev.com>
2016-11-02 20:29:49 +0100

    Fix quotes i array za NEW event

--- admin/eventadd.php
+++ admin/eventadd.php
@@ -3,7 +3,7 @@
 
 //properly encode POST
 foreach ($_POST as $key => $value) {
-    $_POST[$key] = mysqli_real_escape_string($mysqli,$value);
+    if(!is_array($_POST[$key])){$_POST[$key] = mysqli_real_escape_string($mysqli,$value);}
 }
 
 //handle post
