0f169221ccabf4ad67771c3b3cd0856dd1c72af4
mihajlo <mihajlo88@gmail.com>
2016-11-03 13:52:58 +0100

    cross-origin

--- ucesnici.php
+++ ucesnici.php
@@ -1,3 +1,9 @@
+<?php  
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: PUT, GET, POST");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
