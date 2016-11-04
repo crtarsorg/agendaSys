aefeb1ceea2776cc4f12f50aade02d37e0380e34
XoDev <misha@xodev.com>
2016-11-04 15:22:07 +0100

    Access-Control-Allow-Origin za API

--- api/index.php
+++ api/index.php
@@ -1,4 +1,7 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: PUT, GET, POST");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 
 //echo "<pre>";
 //var_dump($_GET[request]);
