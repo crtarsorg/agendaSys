af625c45abda51e63ad31aef63a616c30728bc60
XoDev <misha@xodev.com>
2016-11-08 13:26:12 +0100

    Emptu DB structure

--- admin/agenda.sql
+++ admin/agenda.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.4.11
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Oct 25, 2016 at 02:36 PM
--- Server version: 5.6.25-log
--- PHP Version: 5.6.5
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
@@ -28,22 +19,17 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `events` (
   `eid` int(11) NOT NULL,
-  `ename` tinytext NOT NULL,
+  `ename` tinytext,
   `edesc` tinytext,
   `eldesc` text,
   `edate` date NOT NULL,
   `etime` time NOT NULL,
   `eloc` int(11) NOT NULL,
-  `eact` tinyint(1) NOT NULL DEFAULT '0'
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `events`
---
-
-INSERT INTO `events` (`eid`, `ename`, `edesc`, `eldesc`, `edate`, `etime`, `eloc`, `eact`) VALUES
-(1, 'Collecting documentation', NULL, NULL, '2016-11-01', '08:30:00', 1, 1),
-(2, 'Main Session - Official Welcome', 'Short decs', '<p>				Long desc here</p>', '2016-11-02', '15:00:00', 4, 1);
+  `eact` tinyint(1) NOT NULL DEFAULT '0',
+  `elink` tinytext,
+  `epartneri` text,
+  `etip` varchar(300) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -61,17 +47,7 @@ CREATE TABLE IF NOT EXISTS `locations` (
   `ladres` tinytext NOT NULL,
   `lcity` tinytext NOT NULL,
   `lstate` tinytext
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `locations`
---
-
-INSERT INTO `locations` (`lid`, `lname`, `ldesc`, `lcoordx`, `lcoordy`, `lcolor`, `ladres`, `lcity`, `lstate`) VALUES
-(1, 'Lobi22', 'Lobi na glavnom ulazu22', '44.802472', '20.466342', 'FF0000', 'Trg Nikole Pašića 1332', 'Beograd32', 'Srbija'),
-(2, 'Skupstinska sala br 1', '', '44.802472', '20.466341', '00FF00', 'Trg Nikole Pašića 13', 'Beograd', ''),
-(3, 'name', '<h1><b>				desc&nbsp;</b></h1><p>here</p>', '44.802472', '20.466341', '1024FF', 'adresa', 'grad', 'drzava'),
-(4, 'test lok naziv', '<p>				test opis						</p>', '44.802472', '20.466341', 'FF0000', 'adesa', 'BGD', 'SRB');
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -84,15 +60,7 @@ CREATE TABLE IF NOT EXISTS `s2es` (
   `evtid` int(11) NOT NULL,
   `spkid` int(11) NOT NULL,
   `s2esord` int(11) NOT NULL
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `s2es`
---
-
-INSERT INTO `s2es` (`s2esid`, `evtid`, `spkid`, `s2esord`) VALUES
-(1, 2, 1, 1),
-(2, 2, 2, 2);
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -104,20 +72,10 @@ CREATE TABLE IF NOT EXISTS `speakers` (
   `sid` int(11) NOT NULL,
   `sname` tinytext NOT NULL,
   `sdesc` text,
-  `simg` tinytext
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `speakers`
---
-
-INSERT INTO `speakers` (`sid`, `sname`, `sdesc`, `simg`) VALUES
-(1, 'Aleksandar Vučić', '<p>				Aleksandar Vučić (Beograd, SFRJ, 5. mart 1970) srpski je pravnik i političar, predsednik Vlade Republike Srbije i predsednik Srpske napredne stranke. Bivši je ministar odbrane i ministar za informisanje.&nbsp;\r\n</p><div>Četrnaest godina bio je generalni sekretar Srpske radikalne stranke, a bio je i ministar informisanja u Vladi narodnog jedinstva[1] u Vladi Srbije čiji je premijer bio Mirko Marjanović.						</div>', 'x.png'),
-(2, 'Raša Nedeljkov', '<p>aaa rfgdfg fdhgdfh</p>', '2.png'),
-(3, 'dfgd', '<p>gdg</p>', NULL),
-(4, 'test', '<p>testting</p>', '4.png'),
-(5, 'qq', '<p>wwww</p>', '5.png'),
-(6, 'MDK', '<ul><li><b><i>MDEk DESC</i></b></li></ul>', '6.png');
+  `simg` tinytext,
+  `sorg` tinytext,
+  `stitula` tinytext
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
 -- Indexes for dumped tables
@@ -155,22 +113,22 @@ ALTER TABLE `speakers`
 -- AUTO_INCREMENT for table `events`
 --
 ALTER TABLE `events`
-  MODIFY `eid` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `eid` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `locations`
 --
 ALTER TABLE `locations`
-  MODIFY `lid` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+  MODIFY `lid` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `s2es`
 --
 ALTER TABLE `s2es`
-  MODIFY `s2esid` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+  MODIFY `s2esid` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `speakers`
 --
 ALTER TABLE `speakers`
-  MODIFY `sid` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+  MODIFY `sid` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
